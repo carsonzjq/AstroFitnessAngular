ca345e7712bdfb3b52973dd0eadea0ea32f15db3
carsonzjq <carson.zjq@gmail.com>
2018-03-08 18:44:36 -0500

    fixed bug

---
+++ src/app/trainer.ts
@@ -0,0 +1,13 @@
+
+export class Trainer {
+	
+	constructor(
+		public firstname?: string,
+		public lastname?: string,
+		public address?: string,
+		public email?: string,
+		public password?: string,
+		public home_gym?: Gym
+		) {  }
+
+}
